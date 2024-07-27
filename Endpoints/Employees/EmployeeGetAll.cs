411cf8c3a434a44faf08cceb13fc5674219fba8d
Matheus Gomes <math.s.gomes@gmail.com>
2024-07-27 19:24:28 -0300

    add pagination

--- Endpoints/Employees/EmployeeGetAll.cs
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -1,5 +1,6 @@
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IWantApp.Endpoints.Employees;
 
@@ -9,9 +10,9 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(UserManager<IdentityUser> userManager)
+    public static IResult Action(UserManager<IdentityUser> userManager, int page, int rows = 10)
     {
-        var users = userManager.Users.ToList();
+        var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList();
         var employees = new List<EmployeeResponse>();
         
         foreach (var user in users)
