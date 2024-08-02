f75a61adfaf4702a9c252b0a34d5e63c12bbb59b
Matheus Gomes <math.s.gomes@gmail.com>
2024-08-01 23:26:52 -0300

    add as no tracking

--- Endpoints/Products/ProductGetShowcase.cs
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -12,7 +12,9 @@ public class ProductGetShowcase
         if (row > 10)
             return Results.Problem(title: "Row with max 10", statusCode: 400);
         
-        var queryBase = context.Products.Include(product => product.Category)
+        var queryBase = context.Products
+            .AsNoTracking()
+            .Include(product => product.Category)
             .Where(product => product.HasStock && product.Category.Active);
 
         if (orderBy == "name")
