e4dbb5459e9da57c7357b16b394ee65e5dc0b53d
Matheus Gomes <math.s.gomes@gmail.com>
2024-08-01 22:59:51 -0300

    add validations endpoint product showcase

--- Endpoints/Products/ProductGetShowcase.cs
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -9,13 +9,18 @@ public class ProductGetShowcase
     [AllowAnonymous]
     public static async Task<IResult> Action(ApplicationDbContext context, int page = 1, int row = 10, string orderBy = "name")
     {
+        if (row > 10)
+            return Results.Problem(title: "Row with max 10", statusCode: 400);
+        
         var queryBase = context.Products.Include(product => product.Category)
             .Where(product => product.HasStock && product.Category.Active);
 
         if (orderBy == "name")
             queryBase.OrderBy(product => product.Name);
-        else
+        else if (orderBy == "price")
             queryBase.OrderBy(product => product.Price);
+        else
+            return Results.Problem(title: "OrderBy only by price or name", statusCode: 400);
 
         var queryFilter = queryBase.Skip((page - 1) * row).Take(row);
 
