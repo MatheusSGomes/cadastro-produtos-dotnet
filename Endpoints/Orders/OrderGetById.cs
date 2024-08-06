41a72b999d4dd14e7683015ddffa0ae67e57e737
Matheus Gomes <math.s.gomes@gmail.com>
2024-08-06 00:05:50 -0300

    add order get by id with employee or client

--- Endpoints/Orders/OrderGetById.cs
+++ Endpoints/Orders/OrderGetById.cs
@@ -8,9 +8,17 @@ public class OrderGetById
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    private static async Task<IResult> Action(Guid id, ApplicationDbContext context)
+    private static async Task<IResult> Action(Guid id, ApplicationDbContext context, HttpContext http)
     {
+        var userId = http.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
         var order = await context.Orders.Where(order => order.Id == id).FirstOrDefaultAsync();
+
+        var isNotClient = order.ClientId != userId;
+        var isNotEmployee = http.User.Claims.FirstOrDefault(c => c.Type == "EmployeeCode").Value != null;
+
+        if (isNotClient || isNotEmployee)
+            return Results.BadRequest("Apenas cliente pode ter acesso ao seu pedido");
+
         var orderResponse = new OrderResponse(order.Id, order.ClientId, order.Total, order.DeliveryAddress);
         return Results.Ok(orderResponse);
     }
